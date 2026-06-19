3683c1929e38160d00c4dcee925c8e9fa2220892
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-19 14:22:39 +0200

    Replace static HTML files with EJS templates

--- app.js
+++ app.js
@@ -7,28 +7,43 @@ const options = {
   root: path.join(import.meta.dirname),
 };
 
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use((req, _res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
   next();
 });
 
 app.get("/", (_req, res) => {
-  res.sendFile("index.html", options);
+  res.render("index", {
+    title: "Home",
+    content: "This is a home page",
+  });
 });
 
 app.get("/about", (_req, res) => {
-  res.sendFile("about.html", options);
+  res.render("index", {
+    title: "About",
+    content: "This is a page containing information about somebody",
+  });
 });
 
 app.get("/contact-me", (_req, res) => {
-  res.sendFile("contact-me.html", options);
+  res.render("index", {
+    title: "Contact Me",
+    content: "This is a page with contact information",
+  });
 });
 
 app.use(express.static("public", options));
 
 app.use((_req, res, _next) => {
   console.log(" -> 404");
-  res.status(404).sendFile("404.html", options);
+  res.status(404).render("index", {
+    title: "Error",
+    content: "ERROR: 404 not found",
+  });
 });
 
 app.use((err, _req, res, _next) => {
